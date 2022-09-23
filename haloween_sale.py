67da557219f8648df328a9de8ebf3cc121fed281
Himanshu <ghimanshu98@gmail.com>
2022-09-23 13:56:24 +0530

    completed haloween_sale.py, append_and_delete_pending

--- haloween_sale.py
+++ haloween_sale.py
@@ -1,20 +1,29 @@
-def howManyGames(p, d, m, s):
+def howManyGames(first_item_cost, discount, m, spendable_money):
     # Return the number of games you can buy
-    bill = 0
-    item = 0
-    last_item = 0
-    while(bill <= s):
-        if item == 0:
-            last_item = p
-            bill = bill + p
-            item += 1
-        elif last_item <= m:
-            last_item = last_item -d
-            bill = bill + last_item
-        elif last_item > m:
-            bill = bill + m
+    if spendable_money >= first_item_cost:
+        bill = 0
+        item = 0
+        next_item_cost = 0
+        while(bill < spendable_money):
+            if item==0: # first item
+                item += 1
+                bill = bill + first_item_cost
+                next_item_cost = first_item_cost - discount
+                print(first_item_cost)
+            elif next_item_cost > m:
+                bill = bill + next_item_cost
+                if bill <= spendable_money:
+                    item += 1
+                    print(next_item_cost)
+                next_item_cost = next_item_cost - discount
+            elif next_item_cost <= m:
+                bill = bill +m
+                if bill <= spendable_money:
+                    item += 1
+                    print(m)
+        return item
+    else:
+        return 0
 
-    return item
-
-print(howManyGames(20,3,6,70))
-        
+# print(howManyGames(20,3,6,85))
+print(howManyGames(100,19,1,180))
